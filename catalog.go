940535e23e8eafa3a070c3cdf002ddd74f22c8b0
pbegle <peter.begle@bytecode.io>
2021-03-14 01:07:13 -0800

    Changed Stream.Schema to *Schema datatype instead and ran go fmt.

--- catalog.go
+++ catalog.go
@@ -7,33 +7,33 @@ import (
 
 // Stream in a Catalog
 type Stream struct {
-    TapStreamID string
-    Stream string
-    KeyProperties []string
-    Schema Schema
-    ReplicationKey string
-    ReplicationMethod string
-    IsView bool
-    Database string
-    Table string
-    RowCount int
-    StreamAlias string
-    Metadata string // TODO: make this a struct
+	TapStreamID       string
+	Stream            string
+	KeyProperties     []string
+	Schema            *Schema
+	ReplicationKey    string
+	ReplicationMethod string
+	IsView            bool
+	Database          string
+	Table             string
+	RowCount          int
+	StreamAlias       string
+	Metadata          string // TODO: make this a struct
 }
 
 // Catalog contains streams
 type Catalog struct {
-    Streams []Stream
+	Streams []Stream
 }
 
 // GetStream iterates through slice and returns Stream struct matching stream name
 func (c Catalog) GetStream(streamID string) (Stream, error) {
-    for _, s := range c.Streams {
-        if s.TapStreamID == streamID {
-            return s, nil
-        }
-    }
+	for _, s := range c.Streams {
+		if s.TapStreamID == streamID {
+			return s, nil
+		}
+	}
 
-    errorMessage := fmt.Sprintf("Stream %s not found", streamID)
-    return Stream{}, errors.New(errorMessage)
+	errorMessage := fmt.Sprintf("Stream %s not found", streamID)
+	return Stream{}, errors.New(errorMessage)
 }
--- schema.go
+++ schema.go
@@ -2,21 +2,21 @@ package singer
 
 // Schema defines the shape and properties of a stream
 type Schema struct {
-    Type string `json:"type"`
-    AdditionalProperties bool `json:"additionalProperties"`
-    Properties map[string]Schema `json:"properties"`
-    KeyProperties []string `json:"keyProperties"`
-    Selected bool `json:"selected"`
-    Inclusion string `json:"inclusion"`
-    Description string `json:"description"`
-    Minimum float32 `json:"minimum"`
-    Maximum float32 `json:"maximum"`
-    ExclusiveMinimum float32 `json:"exclusiveMinimum"`
-    ExclusiveMaximum float32 `json:"exclusiveMaximum"`
-    MultipleOf float32 `json:"multipleOf"`
-    MaxLength int `json:"maxLength"`
-    MinLength int `json:"minLength"`
-    Format string `json:"format"`
-    Anyof string `json:"anyOf"`
-    PatternProperties string `json:"patternProperties"`
+	Type                 string            `json:"type"`
+	AdditionalProperties bool              `json:"additionalProperties"`
+	Properties           map[string]Schema `json:"properties"`
+	KeyProperties        []string          `json:"keyProperties"`
+	Selected             bool              `json:"selected"`
+	Inclusion            string            `json:"inclusion"`
+	Description          string            `json:"description"`
+	Minimum              float32           `json:"minimum"`
+	Maximum              float32           `json:"maximum"`
+	ExclusiveMinimum     float32           `json:"exclusiveMinimum"`
+	ExclusiveMaximum     float32           `json:"exclusiveMaximum"`
+	MultipleOf           float32           `json:"multipleOf"`
+	MaxLength            int               `json:"maxLength"`
+	MinLength            int               `json:"minLength"`
+	Format               string            `json:"format"`
+	Anyof                string            `json:"anyOf"`
+	PatternProperties    string            `json:"patternProperties"`
 }
